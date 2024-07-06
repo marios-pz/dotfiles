47a782e905457b4a47b514c03138dc078ca81796
marios-pz <marios-pz@proton.me>
2024-07-06 03:43:50 +0300

    Dunst replaced with mako, lots of AUR dependant packages replaced with natives that can be found in pacman, reduces tons of tools from my workflow. New Wallpaper, removed sway tools and replaced with native hyprland tooling. In addition I got back to fix my hyprland config, after looking into the documentation from up to bottom I've added all the cases for a stable workflow. Last but not least the fedora playbook test has finally been converted to arch, with many packages and dependencies I need to get the job done.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,11 +4,11 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+config.enable_wayland = true
 -- This is where you actually apply your config choices
-
 -- For example, changing the color scheme:
 config.color_scheme = "Batman"
--- config.font = wezterm.font("CozetteHiDpi")
+config.font = wezterm.font("CozetteHiDpi")
 
 -- and finally, return the configuration to wezterm
 return config
