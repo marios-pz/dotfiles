0d090b1b6e467a80c39981e5dd28a42b260d42a6
marios-pz <marios-pz@proton.me>
2026-04-28 12:00:09 +0300

    update waybar and lid up/close

--- .config/hypr/scripts/lid-open.sh
+++ .config/hypr/scripts/lid-open.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 LAPTOP_MON="eDP-1"
+
 hyprctl keyword monitor "$LAPTOP_MON,preferred,auto,1"
+
+# Give Hyprland a moment to register the monitor before sending DPMS on
+sleep 1
+hyprctl dispatch dpms on "$LAPTOP_MON"
