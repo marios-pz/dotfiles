59e58a87f93df4591b501b4232ff0d856d34add2
marios-pz <marios-pz@proton.me>
2026-07-04 00:44:02 +0300

    remove stuff

--- .config/hypr/scripts/lid-open.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-LAPTOP_MON="eDP-1"
-
-hyprctl keyword monitor "$LAPTOP_MON,preferred,auto,1"
-
-# Give Hyprland a moment to register the monitor before sending DPMS on
-sleep 1
-hyprctl dispatch dpms on "$LAPTOP_MON"
